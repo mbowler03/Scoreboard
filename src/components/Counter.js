886675813a8d03e3695d5d01e44d38c4edc4b1b8
Matt Bowler <mbowler03@gmail.com>
2021-12-06 14:54:26 -0500

    change score increment from .5 to 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,9 +5,9 @@ const Counter = ( { index, score, changeScore }) => {
 
     return (
 < div className="counter">
-  < button className="counter-action decrement" onClick={() =>  changeScore(index, -.5)}> - </button>
+  < button className="counter-action decrement" onClick={() =>  changeScore(index, -1)}> - </button>
   <span className="counter-score">{ score }</span>
-  <button className="counter-action increment" onClick={() =>  changeScore(index, .5)}> + </button>
+  <button className="counter-action increment" onClick={() =>  changeScore(index, 1)}> + </button>
 </div>
 );
 }
